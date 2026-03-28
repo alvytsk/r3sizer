1e3e2eacee530d00da69f721bc7cc9b2218fc37d
Alexey Vymyatnin <avymiatnin@gmail.com>
2026-03-28 12:52:08 +0700

    Add github actions

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -5,6 +5,7 @@ import wasm from "vite-plugin-wasm"
 import tailwindcss from "@tailwindcss/vite"
 
 export default defineConfig({
+  base: "/r3sizer/",
   plugins: [react(), wasm(), tailwindcss()],
   worker: {
     plugins: () => [wasm()],
